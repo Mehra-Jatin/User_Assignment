b0c54ecbb28178653d5a2d373e77c2153f3ebea3
Mehra-Jatin <jatinmehra578@gmail.com>
2024-11-10 10:43:46 +0530

    initial commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+function App() {
+  return (
+    <>
+       <div >
+      <Button>Button component using shadcn/ui</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
