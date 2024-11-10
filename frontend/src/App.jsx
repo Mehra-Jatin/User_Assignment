2a5b88b2fb141f29b2a4d9ec30845284c6a40737
Mehra-Jatin <jatinmehra578@gmail.com>
2024-11-10 12:47:10 +0530

    Add:- SignIn page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Button } from "@/components/ui/button"
 import React from "react"
 import {BrowserRouter as Router ,Route,Routes,Navigate} from 'react-router-dom'
 import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
         </Routes>
       </Router>
     </>
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input"
+
 function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleclick=()=>{
+        
+    }
   return (
-    <div>
-      
+    <div className="w-full h-screen flex items-center justify-center bg-slate-200">
+      <Card className="w-[350px] h-auto">
+        <CardHeader className="justify-center items-center">
+          <CardTitle >SignIn</CardTitle>
+          <CardDescription>SignIn using valid credentials</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Email</p>
+            <Input type="email" placeholder="user@example.com" onChange={e=>setEmail(e.target.value)}></Input>
+        </CardContent>
+        <CardContent>
+            <p>Password</p>
+            <Input  type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}></Input>
+        </CardContent>
+        <CardContent >
+            <Button className="mx-24">Sign In</Button>
+        </CardContent>
+        <CardFooter>
+           <p className="px-9">Create a new Account?? <Link to={"/signup"} className="underline cursor-pointer">SignUp</Link></p>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
